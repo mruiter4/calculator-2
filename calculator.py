7779f49ceace11a3525772686be82a9874077a7d
Michelle Ruiter <mruiter4@gmail.com>
2020-04-29 15:48:17 -0700

    Get input, quit calculator, validate numeric input

--- calculator.py
+++ calculator.py
@@ -5,3 +5,28 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+def validate_numeric_input(number):
+    try:
+        number = float(number)
+        return number
+
+    except ValueError:
+        print("Make sure you enter numbers for your equation.")
+        calculate()
+
+def calculate():
+    while True:
+        equation = input("Enter your equation or 'q' to quit: ")
+        equation_tokens = equation.split(" ")
+
+        if equation.upper() == 'Q':
+            print("Goodbye!")
+            break
+
+        operator = equation_tokens[0]
+        num1 = validate_numeric_input(equation_tokens[1])
+        if len(equation_tokens) > 2:
+            num2 = validate_numeric_input(equation_tokens[2])
+
+
+calculate()
